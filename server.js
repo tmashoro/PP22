35e7d5e8e9a21cfe3a92aab6deb77e21098597c3
tmashoro <102840195+tmashoro@users.noreply.github.com>
2022-08-01 23:09:17 -0500

    warehouse

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const exphbs = require('express-handlebars');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+console.log("PID: ", process.pid);
+
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
